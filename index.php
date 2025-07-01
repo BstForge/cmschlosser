e163e281ada41bd39755a94a9f861bfab5030628
BstForge <archimage2869@gmail.com>
2025-06-30 22:36:20 -0500

    Initial commit: project structure

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CMSchlosser Site</title>
+</head>
+<body>
+    <h1>It works!</h1>
+    <p>This is the homepage for <strong>cmschlosser.local</strong></p>
+</body>
+</html>
